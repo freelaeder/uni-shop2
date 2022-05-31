7b4b599b52351b788e84c5e47d5f40b36c3ae436
freelaeder <yanglo67@qq.com>
2022-05-31 20:37:03 +0800

    完成tabBar开发

---
+++ pages/cate/cate.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<h5>cate</h5>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<h5>my</h5>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
