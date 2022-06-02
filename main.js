fe84db3ff3d3e2649d1e1f70e3b4f28b47fe132e
freelaeder <yanglo67@qq.com>
2022-06-02 10:37:07 +0800

    完善分类

--- main.js
+++ main.js
@@ -21,6 +21,15 @@ $http.afterRequest = function(){
 	
 	uni.hideLoading()
 }
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 App.mpType = 'app'
 
 const app = new Vue({
