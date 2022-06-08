3d52a26599c119f364e57ebff6e7369589013462
freelaeder <yanglo67@qq.com>
2022-06-08 14:18:00 +0800

    完成列表数据渲染，及价格小数点处理

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,82 @@
+<template>
+	<view class="goods-item">
+		<!-- 左侧的盒子 -->
+		<view class="goods-item-left">
+			<image :src="item.goods_big_logo || defaultPic" class="goods-pic"></image>
+		</view>
+		<!-- 右侧的盒子 -->
+		<view class="goods-item-right">
+			<!-- 商品的名字 -->
+			<view class="goods-name">
+				{{item.goods_name}}
+			</view>
+			<!-- 商品信息 -->
+			<view class="goods-info-box">
+				<view class="goods-price">
+					¥{{item.goods_price | toFixed}}
+				</view>
+			</view>
+
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "my-goods",
+		data() {
+			return {
+				//  默认图片
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+
+			};
+		},
+		props: {
+			item: {
+				type: Object,
+				default: {}
+			}
+		},
+		filters:{
+			// 把数字处理为带有两位小数的数组
+			toFixed(num){
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.goods-item {
+		display: flex;
+		padding: 5px 10px;
+		border-bottom: 1px solid #efefef;
+
+		.goods-item-left {
+			margin: 5px;
+
+			.goods-pic {
+				width: 100px;
+				height: 100px;
+				display: block;
+
+			}
+		}
+
+		.goods-item-right {
+			font-size: 13px;
+			display: flex;
+			justify-content: space-between;
+			flex-direction: column;
+
+			.goods-name {}
+
+			.goods-info-box {
+				.goods-price {
+					color: red;
+				}
+			}
+		}
+	}
+</style>
--- subpkg/good_list/good_list.vue
+++ subpkg/good_list/good_list.vue
@@ -1,6 +1,11 @@
 <template>
 	<view>
-		<h4>good_list</h4>
+		<view class="goods-list">
+			<block v-for="(item,i) in goodsList" :key="i">
+				<my-goods :item="item"></my-goods>
+			</block>
+
+		</view>
 	</view>
 </template>
 
@@ -8,12 +13,74 @@
 	export default {
 		data() {
 			return {
-				
+				// 请求参数
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: '1',
+					pagesize: '20',
+
+				},
+				// 商品列表数据
+				goodsList: [],
+				total: 0,
+				//  默认图片
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+
 			};
+		},
+		onLoad(options) {
+			// 页面加载时
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
+			// 获取商品列表数据
+			this.getGoodsList()
+		},
+		methods: {
+			// 获取商品数据的方法
+			async getGoodsList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 赋值
+				this.goodsList = res.message.goods
+				this.total = res.message.total
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	.goods-list {
+		.goods-item {
+			display: flex;
+			padding: 5px 10px;
+			border-bottom: 1px solid #efefef;
+			
+			.goods-item-left {
+				margin: 5px;
+				.goods-pic {
+					width: 100px;
+					height: 100px;
+					display: block;
+					
+				}
+			}
 
+			.goods-item-right {
+				font-size: 13px;
+				display: flex;
+				justify-content: space-between;
+				flex-direction: column;
+				.goods-name {}
+
+				.goods-info-box {
+					.goods-price {
+						color: red;
+					}
+				}
+			}
+		}
+	}
 </style>
