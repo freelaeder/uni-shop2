4c5ab90496b8ea0bc2fc676c375f7a7f29e22387
freelaeder <yanglo67@qq.com>
2022-06-09 17:35:40 +0800

    完成数据列表功能开发

--- subpkg/good_list/good_list.vue
+++ subpkg/good_list/good_list.vue
@@ -1,9 +1,10 @@
 <template>
 	<view>
 		<view class="goods-list">
-			<block v-for="(item,i) in goodsList" :key="i">
+			<!-- // block 不支持点击事件 -->
+			<view v-for="(item,i) in goodsList" :key="i" @click="gotoDetail(item)">
 				<my-goods :item="item"></my-goods>
-			</block>
+			</view>
 
 		</view>
 	</view>
@@ -25,7 +26,9 @@
 				goodsList: [],
 				total: 0,
 				//  默认图片
-				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+				// 是否正在请求数据
+				isloading:false,
 
 			};
 		},
@@ -38,15 +41,50 @@
 		},
 		methods: {
 			// 获取商品数据的方法
-			async getGoodsList() {
+			async getGoodsList(cb) {
+				// 打开节流阀
+				this.isloading = true
 				const {
 					data: res
 				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
 				if (res.meta.status !== 200) return uni.$showMsg()
+				// 关闭节流阀
+				this.isloading= false
+				cb && cb()
 				// 赋值
-				this.goodsList = res.message.goods
+				console.log(res,'-------------------------------');
+				// 拼接旧数据 和新数据
+				this.goodsList = [...this.goodsList, ...res.message.goods]
 				this.total = res.message.total
+			},
+			// 点击进入详情页
+			gotoDetail(e){
+				console.log(e.goods_id,'000000000000000');
+				uni.navigateTo({
+					url:'/subpkg/good_detail/good_detail?goods_id='+e.goods_id
+				})
 			}
+		},
+		// 上拉触底事件
+		onReachBottom() {
+			// 判断是否加载完毕
+			if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据已经加载完了哦')
+			// 判断节流阀的状态
+			if(this.isloading) return
+			// 页码加一
+			this.queryObj.pagenum++
+			this.getGoodsList()
+		},
+		// 下拉刷新
+		onPullDownRefresh() {
+			// 重置关键数据
+			this.queryObj.pagenum= 1
+			this.total=0
+			this.isloading = false
+			this.goodsList=[]
+			
+			// 重新发起请求
+			this.getGoodsList(()=>uni.stopPullDownRefresh())
 		}
 	}
 </script>
@@ -57,14 +95,15 @@
 			display: flex;
 			padding: 5px 10px;
 			border-bottom: 1px solid #efefef;
-			
+
 			.goods-item-left {
 				margin: 5px;
+
 				.goods-pic {
 					width: 100px;
 					height: 100px;
 					display: block;
-					
+
 				}
 			}
 
@@ -73,6 +112,7 @@
 				display: flex;
 				justify-content: space-between;
 				flex-direction: column;
+
 				.goods-name {}
 
 				.goods-info-box {
--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -956,7 +956,7 @@ function initData(vueOptions, context) {
     try {
       data = data.call(context); // 支持 Vue.prototype 上挂的数据
     } catch (e) {
-      if (Object({"NODE_ENV":"development","VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.warn('根据 Vue 的 data 函数初始化小程序 data 失败，请尽量确保 data 函数中不访问 vm 对象，否则可能影响首次数据渲染速度。', data);
       }
     }
@@ -8662,7 +8662,7 @@ function type(obj) {
 
 function flushCallbacks$1(vm) {
     if (vm.__next_tick_callbacks && vm.__next_tick_callbacks.length) {
-        if (Object({"NODE_ENV":"development","VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
+        if (Object({"VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:flushCallbacks[' + vm.__next_tick_callbacks.length + ']');
@@ -8683,14 +8683,14 @@ function nextTick$1(vm, cb) {
     //1.nextTick 之前 已 setData 且 setData 还未回调完成
     //2.nextTick 之前存在 render watcher
     if (!vm.__next_tick_pending && !hasRenderWatcher(vm)) {
-        if(Object({"NODE_ENV":"development","VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:nextVueTick');
         }
         return nextTick(cb, vm)
     }else{
-        if(Object({"NODE_ENV":"development","VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance$1 = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance$1.is || mpInstance$1.route) + '][' + vm._uid +
                 ']:nextMPTick');
@@ -8776,7 +8776,7 @@ var patch = function(oldVnode, vnode) {
     });
     var diffData = this.$shouldDiffData === false ? data : diff(data, mpData);
     if (Object.keys(diffData).length) {
-      if (Object({"NODE_ENV":"development","VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"VUE_APP_NAME":"uni-shop2","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + this._uid +
           ']差量更新',
           JSON.stringify(diffData));
@@ -9661,7 +9661,58 @@ function resolveLocaleChain(locale) {
 
 /***/ }),
 
-/***/ 68:
+/***/ 85:
+/*!********************************************************************************************!*\
+  !*** E:/mwep/uni-shop2/uni_modules/uni-search-bar/components/uni-search-bar/i18n/index.js ***!
+  \********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _en = _interopRequireDefault(__webpack_require__(/*! ./en.json */ 86));
+var _zhHans = _interopRequireDefault(__webpack_require__(/*! ./zh-Hans.json */ 87));
+var _zhHant = _interopRequireDefault(__webpack_require__(/*! ./zh-Hant.json */ 88));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}var _default =
+{
+  en: _en.default,
+  'zh-Hans': _zhHans.default,
+  'zh-Hant': _zhHant.default };exports.default = _default;
+
+/***/ }),
+
+/***/ 86:
+/*!*******************************************************************************************!*\
+  !*** E:/mwep/uni-shop2/uni_modules/uni-search-bar/components/uni-search-bar/i18n/en.json ***!
+  \*******************************************************************************************/
+/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"Search enter content\"}");
+
+/***/ }),
+
+/***/ 87:
+/*!************************************************************************************************!*\
+  !*** E:/mwep/uni-shop2/uni_modules/uni-search-bar/components/uni-search-bar/i18n/zh-Hans.json ***!
+  \************************************************************************************************/
+/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"请输入搜索内容\"}");
+
+/***/ }),
+
+/***/ 88:
+/*!************************************************************************************************!*\
+  !*** E:/mwep/uni-shop2/uni_modules/uni-search-bar/components/uni-search-bar/i18n/zh-Hant.json ***!
+  \************************************************************************************************/
+/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"請輸入搜索內容\"}");
+
+/***/ }),
+
+/***/ 96:
 /*!*****************************************************************************!*\
   !*** E:/mwep/uni-shop2/uni_modules/uni-icons/components/uni-icons/icons.js ***!
   \*****************************************************************************/
@@ -10835,57 +10886,6 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
     "font_class": "cart",
     "unicode": "e631",
     "unicode_decimal": 58929 }] };exports.default = _default;
-
-/***/ }),
-
-/***/ 87:
-/*!********************************************************************************************!*\
-  !*** E:/mwep/uni-shop2/uni_modules/uni-search-bar/components/uni-search-bar/i18n/index.js ***!
-  \********************************************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _en = _interopRequireDefault(__webpack_require__(/*! ./en.json */ 88));
-var _zhHans = _interopRequireDefault(__webpack_require__(/*! ./zh-Hans.json */ 89));
-var _zhHant = _interopRequireDefault(__webpack_require__(/*! ./zh-Hant.json */ 90));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}var _default =
-{
-  en: _en.default,
-  'zh-Hans': _zhHans.default,
-  'zh-Hant': _zhHant.default };exports.default = _default;
-
-/***/ }),
-
-/***/ 88:
-/*!*******************************************************************************************!*\
-  !*** E:/mwep/uni-shop2/uni_modules/uni-search-bar/components/uni-search-bar/i18n/en.json ***!
-  \*******************************************************************************************/
-/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
-/***/ (function(module) {
-
-module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"Search enter content\"}");
-
-/***/ }),
-
-/***/ 89:
-/*!************************************************************************************************!*\
-  !*** E:/mwep/uni-shop2/uni_modules/uni-search-bar/components/uni-search-bar/i18n/zh-Hans.json ***!
-  \************************************************************************************************/
-/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
-/***/ (function(module) {
-
-module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"请输入搜索内容\"}");
-
-/***/ }),
-
-/***/ 90:
-/*!************************************************************************************************!*\
-  !*** E:/mwep/uni-shop2/uni_modules/uni-search-bar/components/uni-search-bar/i18n/zh-Hant.json ***!
-  \************************************************************************************************/
-/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
-/***/ (function(module) {
-
-module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"請輸入搜索內容\"}");
 
 /***/ })
 
